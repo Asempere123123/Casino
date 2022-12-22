d5d11a9a6e92c9f66f2167d54a5afb894aacc361
Asempere123123 <asempere@colegioaleman.net>
2022-12-22 11:43:48 +0100

    asd

--- BlackJack/Cartas_probalilidad.py
+++ BlackJack/Cartas_probalilidad.py
@@ -1,6 +1,10 @@
 import random
-iteraciones = 10000000
+iteraciones = 1000000
 media = 0
+mediaValor = 0
+
+cant12 = 0
+val12 = 0
 
 sumCarta = {"1" : 0, "2" : 0, "3" : 0, "4" : 0, "5" : 0, "6" : 0, "7" : 0, "8" : 0, "9" : 0, "10" : 0, "11" : 0, "12" : 0, "13" : 0, "14" : 0, "15" : 0, "16" : 0, "17" : 0, "18" : 0, "19" : 0, "20" : 0, "21" : 0}
 vecesCarta = {"1" : 0, "2" : 0, "3" : 0, "4" : 0, "5" : 0, "6" : 0, "7" : 0, "8" : 0, "9" : 0, "10" : 0, "11" : 0, "12" : 0, "13" : 0, "14" : 0, "15" : 0, "16" : 0, "17" : 0, "18" : 0, "19" : 0, "20" : 0, "21" : 0}
@@ -17,12 +21,21 @@ for i in range(0, iteraciones):
         o += 1
         pillaCartas += Cartas[-o]
         if o == 2:
+            mediaValor += Cartas[-(o+1)]/iteraciones
+
+            if (pillaCartas == 12):
+                cant12+=1
+                val12 += Cartas[-(o+1)]
+
             if pillaCartas == 22:
                 pillaCartas -= 10
             es = str(pillaCartas)
             vecesCarta[es] += 1
-    
-    sumCarta[es] += pillaCartas
+            
+    if (pillaCartas <22):
+        sumCarta[es] += pillaCartas
+    else:
+        vecesCarta[es] -=1
     media += pillaCartas/iteraciones
 
 carta = []
@@ -35,4 +48,5 @@ for a, v in sumCarta.items():
     i += 1
 
 print(carta)
-print(media)
+print(media, mediaValor)
+print(val12/cant12)
